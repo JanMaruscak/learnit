91f4aa30ba70fb922842691b36aec875ee2afae1
maruscak.jan <maruscak.jan@ssakhk.cz>
2019-12-05 20:58:59 +0100

    Logo yay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../logo.svg";
+import Logo from "../images/logo.svg";
 
 function Navbar() {
   const [mobile, setMobile] = useState(false);
